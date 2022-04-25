a0c60b32e2403b48a2cea50520265bcd34d92942
Jacky <f4111222@hotmail.com>
2022-04-25 16:24:39 +0800

    liquidate practice

--- contracts/JToken.sol
+++ contracts/JToken.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract JToken is ERC20 {
-   constructor(uint256 initialSupply) public ERC20("JToken", "JT") {
+   constructor() public ERC20("JToken", "JT") {
+       uint256 initialSupply = 10000000000000 *10**uint256(8);
         _mint(msg.sender, initialSupply);
     }
 }
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -26,7 +26,39 @@ async function main() {
 
   console.log("Interestrate deployed to:", interestrate.address);
 
-  // Deploy CEther 
+  // deploy Testcoin: JToken
+  const Testcoin = await ethers.getContractFactory("JToken");
+  const testcoin = await Testcoin.deploy();
+  await testcoin.deployed();  
+
+  console.log('Testcoin: JToken deployed to:', testcoin.address);
+
+  // deploy cERC20 Delegate
+  const CErc20DelegateFactory = await ethers.getContractFactory('CErc20Delegate');
+  cErc20DelegateContract = await CErc20DelegateFactory.deploy();
+  await cErc20DelegateContract.deployed();
+  
+  console.log('CErc20DelegateContract deployed to:', cErc20DelegateContract.address);
+
+  // deploy cERC20
+  const CErc20 = await ethers.getContractFactory('CErc20Delegator');
+  cErc20 = await CErc20.deploy(
+    testErc20Contract.address,
+    comptrollerContract.address,
+    interestRateModelContract.address,
+    ethers.utils.parseUnits('1', 18),
+    '',
+    'cErc20',
+    1,
+    deployer.address,
+    cErc20DelegateContract.address,
+    []
+  );
+  await cerc20.deployed();
+
+  console.log('CErc20 to:', cErc20.address);
+
+  // deploy cETH
   const CEther = await ethers.getContractFactory("CEther");
   const cEther = await CEther.deploy(
     comptroller.address, 
@@ -38,28 +70,35 @@ async function main() {
     deployer.address
   );
   await cEther.deployed();  
-
+  
   console.log('CEther deployed to:', cEther.address);
 
-   // Deploy oracle
-   const Oracle = await hre.ethers.getContractFactory("SimplePriceOracle");
-   const oracle = await Oracle.deploy();
-   await oracle.deployed();
+
+  // deploy oracle
+  const Oracle = await hre.ethers.getContractFactory("SimplePriceOracle");
+  const oracle = await Oracle.deploy();
+  await oracle.deployed();
      
-   console.log("Oracle deployed to:", oracle.address);
-     
-   // Set oracle
-  await comptroller._setPriceOracle(oracle.address);
-  await oracle.setDirectPrice(cEther.address, 20000);
+  console.log("Oracle deployed to:", oracle.address);
+  
 
   // Support CEther in componud  
   await comptroller._supportMarket(cEther.address);
-  // Enter markets
-  await comptroller.enterMarkets([cEther.address]);
+  await comptroller._supportMarket(cErc20.address);
+
+  await Testcoin.approve(cErc20.address, ethers.utils.parseUnits('100000', 18));
+  await Testcoin.connect(borrower).approve(cErc20.address, ethers.utils.parseUnits('100000', 18));
+
+
+  // Set oracle & origin JToken price to 10
+  await oracle.setDirectPrice(cEther.address, ethers.utils.parseUnits('10', 18));
+  await comptroller._setPriceOracle(oracle.address);
+
   // Set Collateral Factor
   await comptroller._setCollateralFactor(cEther.address, ethers.utils.parseEther("1"));
-  
+  await comptroller._setCollateralFactor(cErc20.address, ethers.utils.parseEther("1"));
 
+/*
   //Start to test mint/redeem/borrow/repay
 
   await cEther.mint({ value: BigNumber.from(1).mul(10 ** 8) });
@@ -73,7 +112,27 @@ async function main() {
   await cEther.redeem(BigNumber.from(2).mul(10 ** 6));
   balance = await cEther.balanceOf(deployer.address);
   console.log('cEther balance after redeem', balance.toString());
+*/
 
+  // Start to test liquidate
+  // provide 100 JToken
+  await cErc20.mint(ethers.utils.parseUnits('100', 18));
+
+  // borrower provide 100 cETH
+  await cEther.connect(borrower).mint({ value: ethers.utils.parseUnits('100', 18) });
+  
+  // borrower Enter markets
+  await comptroller.connect(borrower).enterMarkets([cEther.address]);
+
+  // borrower lend 10000 JToken
+  await cErc20.connect(borrower).borrow(ethers.utils.parseUnits('10000', 18));
+
+  //set new JToken price to 50000
+  await priceOracleContract.setDirectPrice(Testcoin.address, ethers.utils.parseUnits('50000', 18));
+
+  //liquidate borrower
+  await cErc20.liquidateBorrow(borrower.address, ethers.utils.parseUnits('10000', 18), cEther.address);
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
